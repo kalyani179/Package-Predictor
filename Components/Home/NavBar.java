56a36bf7cdbc5366e8117da50a4b7955f6955cb2
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-05-18 18:59:25 +0530

    selection panel displaying bug fixed

--- Components/Home/NavBar.java
+++ Components/Home/NavBar.java
@@ -12,21 +12,27 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
-class CurvyButton extends JButton{
-    CurvyButton(String text){
+
+
+class CurvyButton extends JButton {
+    CurvyButton(String text) {
         this.setBackground(Color.white);
         this.setOpaque(true);
         this.setBorder(new EmptyBorder(10,10,10,10));
-        this.setFont(new Font("Serif",Font.BOLD,24));
+        this.setFont(new Font("Serif", Font.BOLD, 24));
         this.setText(text);
     }
 }
-class SelectionPanel extends JPanel{
-    public static int width = Constants.width-200;
-    public static int height = Constants.height/2;
-    SelectionPanel(){
+
+
+class SelectionPanel extends JPanel {
+    public static int width = Constants.width - 200;
+    public static int height = Constants.height / 12;
+
+    SelectionPanel() {
         this.setBackground(Color.black);
         this.setForeground(Color.white);
+        this.setLayout(null); // Using null layout for absolute positioning
 
         CurvyButton ind = new CurvyButton("Individual");
         ind.setBounds(0,0,SelectionPanel.width/3,SelectionPanel.height);
@@ -36,8 +42,9 @@ class SelectionPanel extends JPanel{
         this.add(grp);
     }
 }
-class Hamburger extends JLabel{
-    Hamburger(){
+
+class Hamburger extends JLabel {
+    Hamburger() {
         ImageIcon hamburgerIcon = new ImageIcon("./Assets/Menu.png");
         
         Image img = hamburgerIcon.getImage();
@@ -50,27 +57,31 @@ class Hamburger extends JLabel{
         this.setOpaque(true);
     }
 }
+
 public class NavBar extends JPanel {
-    public NavBar(){
+    public NavBar() {
         this.setLayout(null);
         this.setBackground(Color.BLACK);
-        this.setPreferredSize(new Dimension(Constants.width,300));
+        this.setPreferredSize(new Dimension(Constants.width, 300));
+        getLogo();
     }
+
     public void getLogo() {
         JLabel logo = new JLabel("SatKal");
         Hamburger menu = new Hamburger();
-        menu.setBounds(Constants.width - 80,10,50,50);
+        menu.setBounds(Constants.width - 80, 10, 50, 50);
 
         SelectionPanel sp = new SelectionPanel();
-        this.setBounds(50,500,SelectionPanel.width,SelectionPanel.height);
+        sp.setBounds(50, 50, SelectionPanel.width, SelectionPanel.height); // Adjust position within NavBar
 
         logo.setForeground(Color.white);
         logo.setBackground(Color.black);
-        logo.setBorder(new EmptyBorder(20,20,20,20));
+        logo.setBorder(new EmptyBorder(20, 20, 20, 20));
         logo.setFont(new Font("Serif", Font.BOLD, 48));
         logo.setOpaque(true);
-        logo.setBounds(5, 5, 250, 50); 
-        this.add(logo); 
+        logo.setBounds(5, 5, 250, 50);
+
+        this.add(logo);
         this.add(menu);
         this.add(sp);
     }
