70bc97b0745e82b9e89d31d7807bd79cb723085c
SATHVIK SVS <svssathvik77@gmail.com>
2024-05-18 15:28:48 +0530

    Selection panel not being displayed

--- Components/Home/NavBar.java
+++ Components/Home/NavBar.java
@@ -3,6 +3,7 @@ package Components.Home;
 import Constants.Constants;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,14 +12,37 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+class CurvyButton extends JButton{
+    CurvyButton(String text){
+        this.setBackground(Color.white);
+        this.setOpaque(true);
+        this.setBorder(new EmptyBorder(10,10,10,10));
+        this.setFont(new Font("Serif",Font.BOLD,24));
+        this.setText(text);
+    }
+}
+class SelectionPanel extends JPanel{
+    public static int width = Constants.width-200;
+    public static int height = Constants.height/2;
+    SelectionPanel(){
+        this.setBackground(Color.black);
+        this.setForeground(Color.white);
 
+        CurvyButton ind = new CurvyButton("Individual");
+        ind.setBounds(0,0,SelectionPanel.width/3,SelectionPanel.height);
+        CurvyButton grp = new CurvyButton("Mass/Group");
+        grp.setBounds(SelectionPanel.width/3,0,SelectionPanel.width/2,SelectionPanel.height);
+        this.add(ind);
+        this.add(grp);
+    }
+}
 class Hamburger extends JLabel{
     Hamburger(){
         ImageIcon hamburgerIcon = new ImageIcon("./Assets/Menu.png");
         
         Image img = hamburgerIcon.getImage();
         Image resizedImg = img.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
-        hamburgerIcon = new ImageIcon(resizedImg);
+        hamburgerIcon = new ImageIcon(resizedImg);        
         
         this.setBackground(Color.black);
         this.setForeground(Color.WHITE);
@@ -36,6 +60,10 @@ public class NavBar extends JPanel {
         JLabel logo = new JLabel("SatKal");
         Hamburger menu = new Hamburger();
         menu.setBounds(Constants.width - 80,10,50,50);
+
+        SelectionPanel sp = new SelectionPanel();
+        this.setBounds(50,500,SelectionPanel.width,SelectionPanel.height);
+
         logo.setForeground(Color.white);
         logo.setBackground(Color.black);
         logo.setBorder(new EmptyBorder(20,20,20,20));
@@ -44,5 +72,6 @@ public class NavBar extends JPanel {
         logo.setBounds(5, 5, 250, 50); 
         this.add(logo); 
         this.add(menu);
+        this.add(sp);
     }
 }
